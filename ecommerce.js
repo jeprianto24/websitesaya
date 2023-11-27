7fee6ad9adcd10c2110da4b46965ae1ccb4d261e
jeprianto24 <115442691+jeprianto24@users.noreply.github.com>
2023-11-27 11:47:23 +0800

    Add files via upload

---
+++ ecommerce.js
@@ -0,0 +1,5 @@
+const alertButton = document.getElementById("button");
+alertButton.addEventListener('click', function() {
+alert('Kirim pesan ke whatsapp admin untuk memesan atau menanyakan sesuatu');
+});
+
